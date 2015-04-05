cfafde2cdb8f484d1e9032cf20ecd785559b29bd
stefanbrakovic <brakovic.stefan@gmail.com>
2015-04-06 00:43:46 +0200

    Pocet rad testova al mi se spava nastavljam sutra

---
+++ Twitter/src/test/testTwitPoruke.java
@@ -0,0 +1,27 @@
+package test;
+
+import junit.framework.TestCase;
+
+public class testTwitPoruke extends TestCase {
+
+	public void testGetKorisnik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetKorisnik() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testGetPoruka() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testSetPoruka() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
